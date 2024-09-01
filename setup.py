75f0be7f6fea7345ca1e0ae8c468b87da393f0ea
aaru111 <85352740+aaru111@users.noreply.github.com>
2024-09-01 21:46:58 +0530

    Update setup.py

--- setup.py
+++ setup.py
@@ -1,15 +1,15 @@
 from setuptools import setup, find_packages
 
 setup(
-    name='file-organizer',  # Name of your package
-    version='1.0.0',  # Version of your package
-    author='Your Name',  # Replace with your name
-    author_email='your.email@example.com',  # Replace with your email
+    name='file-organizer',  
+    version='1.0.0',  
+    author='Aarav',  
+    author_email='aarav12303@gmail.com',  
     description='A versatile file organization tool with both CLI and GUI interfaces.',
-    long_description=open('README.md').read(),  # This will be displayed on PyPI
+    long_description=open('README.md').read(),  
     long_description_content_type='text/markdown',
-    url='https://github.com/yourusername/file-organizer',  # Replace with your GitHub repo URL
-    packages=find_packages(include=['modules', 'modules.*']),  # Automatically find packages in modules folder
+    url='https://github.com/aaru111/file-organizer', 
+    packages=find_packages(include=['modules', 'modules.*']),
     include_package_data=True,
     install_requires=[
         'prompt-toolkit==3.0.38',
@@ -21,10 +21,10 @@ setup(
         'License :: OSI Approved :: MIT License',
         'Operating System :: OS Independent',
     ],
-    python_requires='>=3.7',  # Minimum Python version required
+    python_requires='>=3.7', 
     entry_points={
         'console_scripts': [
-            'file-organizer=main:main',  # This allows running `file-organizer` from command line
+            'file-organizer=main:main',  
         ],
     },
 )
